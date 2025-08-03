723800e5540f9d8dcfd6570f64ba0a4d277e623e
sucharita4867 <sucharitasardar7@gmail.com>
2025-08-03 23:18:09 +0530

    added practice task 5

--- assiment-3/problem4.js
+++ assiment-3/problem4.js
@@ -1,14 +1,11 @@
 /** Problem 04 - (Delete / Store) */
-var fileName = "data.pdf";
-for (var i = 0; i === fileName; i++) {
-  console.log(i);
-  if (fileName == "pdf") {
-    console.log("Store");
-  } else if (fileName == "#") {
-    console.log("Store");
-  } else if (fileName === "docx") {
-    console.log("Store");
-  } else {
-    console.log("Delete");
-  }
+var fileName = "#pdfData.jpg";
+if (fileName.includes(".pdf")) {
+  console.log("Store");
+} else if (fileName.includes("#")) {
+  console.log("Store");
+} else if (fileName.includes(".docx")) {
+  console.log("Store");
+} else {
+  console.log("Delete");
 }
---
+++ assiment-3/problem5.js
@@ -0,0 +1,4 @@
+/** Problem 05 - ( PH Email Generator )  */
+var student = { name: "jhankar", roll: 1014, department: "cse" };
+var result = name + roll + "." + department + "@ph.ac.bd";
+console.log(result);
