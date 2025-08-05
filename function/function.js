1040e5366f1b025ce59cd0ad8821b2ec0e468496
sucharita4867 <sucharitasardar7@gmail.com>
2025-08-05 12:47:52 +0530

    added multipli and share task

--- function/function.js
+++ function/function.js
@@ -1,11 +1,24 @@
+// +++++++++++++++++++++++++++++++++++++++++++
 // function jog(num1, num2) {
 //   let sum = num1 + num2;
 //   console.log("sum no:", sum);
 // }
 // jog(26, 47);
-
-function mainus(num1, num2) {
-  let sum = num1 - num2;
-  console.log("mainus no:", sum);
+// ---------------------------------------------
+// function mainus(num1, num2) {
+//   let sum = num1 - num2;
+//   console.log("mainus no:", sum);
+// }
+// mainus(23, 12);
+// ******************************************
+// function multipli(num1, num2) {
+//   sum = num1 * num2;
+//   console.log("multipli no:", sum);
+// }
+// multipli(22, 634);
+////////////////////////////////////////////////
+function share(num1, num2) {
+  total = num1 / num2;
+  console.log("share no:", total);
 }
-mainus(23, 12);
+share(2, 2);
