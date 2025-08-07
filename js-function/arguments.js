3578ec7e91feb880077958ddd1a7567d0df5cb9c
sucharita4867 <sucharitasardar7@gmail.com>
2025-08-07 23:47:21 +0530

    added this problem solving

--- js-function/arguments.js
+++ js-function/arguments.js
@@ -2,9 +2,17 @@ function double(number) {
   const total = number * number;
   console.log(number, total);
 }
-double(6);
-console.log("-------------------");
-double(8);
-console.log("-------------------");
-double(12);
-console.log("-------------------");
+// double(6);
+// console.log("-------------------");
+// double(8);
+// console.log("-------------------");
+// double(12);
+// console.log("-------------------");
+
+function add(num1, num2) {
+  const rasult = num1 - num2;
+  console.log(rasult);
+}
+const fatherAge = 50;
+const myAge = 10;
+add(fatherAge, myAge);
