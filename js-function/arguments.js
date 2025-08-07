a99f0abb37e6477785c22dccafa596d718091a36
sucharita4867 <sucharitasardar7@gmail.com>
2025-08-07 23:40:23 +0530

    added today function practice

---
+++ js-function/arguments.js
@@ -0,0 +1,10 @@
+function double(number) {
+  const total = number * number;
+  console.log(number, total);
+}
+double(6);
+console.log("-------------------");
+double(8);
+console.log("-------------------");
+double(12);
+console.log("-------------------");
---
+++ js-function/paramiter.js
@@ -0,0 +1,18 @@
+function square(number) {
+  //   console.log(square * square);
+  const total = number * number;
+  console.log(total);
+}
+// square(3);
+// square(12);
+// console.log("------------------------");
+// square(89);
+// square(231);
+// square(14325);
+
+function add(a, b, c, d, g) {
+  const total = a + b + c + d - g;
+  console.log(total);
+}
+// add(3, 7, 8, 1, 9);
+// add(437, 655, 679, 129, 98);
