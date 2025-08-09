489b230fe2050b6b3d58b7f76b4d16412c657600
sucharita4867 <sucharitasardar7@gmail.com>
2025-08-09 12:57:47 +0530

    task numbers 5 is complete

--- js-function/practice-task.js
+++ js-function/practice-task.js
@@ -1,20 +1,47 @@
-// practice-task 1
-function number(a, b, c, d) {
-  const total = a * b * c * d;
-  return total;
-}
+// -----------------------------------practice-task 1
+// function number(a, b, c, d) {
+//   const total = a * b * c * d;
+//   return total;
+// }
 // const result = number(23, 41, 18, 39);
 // console.log(result);
 
-// practice task 2
-let total = 0;
-function numbers(num) {
-  if (num % 2 === 1) {
-    total = num * 2;
+// --------------------------------practice task 2
+// let total = 0;
+// function numbers(num) {
+//   if (num % 2 === 1) {
+//     total = num * 2;
+//   } else {
+//     total = num / 2;
+//   }
+//   return total;
+// }
+// const result = numbers(81);
+// console.log(result);
+
+// ----------------------------------practice task 3 (incomplete)
+// ----------------------------------practice task 4(incomplete)
+// const total = [];
+// function stringIntiger(str) {
+//   for (const string of str) {
+//     console.log(string);
+//     if (string === 0) {
+//       total.push(string);
+//     } else {
+//       console.log(string);
+//     }
+//     return total;
+//   }
+// }
+// const result = stringIntiger("30040");
+// console.log(result);
+// ---------------------------------------practice task 5
+function even_odd(nums) {
+  if (nums % 2 === 0) {
+    return "Even No";
   } else {
-    total = num / 2;
+    return "Odd No";
   }
-  return total;
 }
-const result = number(8);
+const result = even_odd(52);
 console.log(result);
