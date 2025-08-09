a55285d793e4343a7c6542fd4390cfab381c5697
sucharita4867 <sucharitasardar7@gmail.com>
2025-08-09 12:43:26 +0530

    added function practice task 1 and 2

---
+++ js-function/practice-task.js
@@ -0,0 +1,20 @@
+// practice-task 1
+function number(a, b, c, d) {
+  const total = a * b * c * d;
+  return total;
+}
+// const result = number(23, 41, 18, 39);
+// console.log(result);
+
+// practice task 2
+let total = 0;
+function numbers(num) {
+  if (num % 2 === 1) {
+    total = num * 2;
+  } else {
+    total = num / 2;
+  }
+  return total;
+}
+const result = number(8);
+console.log(result);
