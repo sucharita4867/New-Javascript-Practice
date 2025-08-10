f4704c4d9d477451a85d2bbb65df28ffa773595b
sucharita4867 <sucharitasardar7@gmail.com>
2025-08-10 23:09:30 +0530

    complete min height practice task

--- js-problem-part2/tallest.js
+++ js-problem-part2/tallest.js
@@ -1,6 +1,27 @@
-const height = [23, 54, 67, 12, 78, 98, 34, 92];
+const height = [23, 154, 67, 12, 78, 98, 34, 92];
 function maxHeight(numbers) {
-  console.log(numbers);
+  let max = numbers[0];
+  for (const num of numbers) {
+    if (num > max) {
+      max = num;
+    }
+  }
+  return max;
 }
-const result = maxHeight(height);
+// const result = maxHeight(height);
+// console.log(result);
+
+// home work min
+const heights = [23, 15, 4, 7, 12, 78, 98, 34, 2];
+
+function minHeight(numbers) {
+  let minNo = numbers[0];
+  for (const numb of numbers) {
+    if (numb < minNo) {
+      minNo = numb;
+    }
+  }
+  return minNo;
+}
+const result = minHeight(heights);
 console.log(result);
