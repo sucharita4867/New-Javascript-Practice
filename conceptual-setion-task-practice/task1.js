506aa7ecb121b92723ac80a0677de36959ca6b93
sucharita4867 <sucharitasardar7@gmail.com>
2025-08-12 22:04:25 +0530

    problem solving task 1 is complete

--- conceptual-setion-task-practice/task1.js
+++ conceptual-setion-task-practice/task1.js
@@ -2,12 +2,17 @@ function sum(num1, num2) {
   let result = num1 + num2;
   return result;
 }
-const outputSum = sum(2, 4);
-console.log(outputSum);
+// const outputSum = sum(2, 4);
+// console.log(outputSum);
 
 function multiply(num1, num2) {
   let result = num1 * num2;
   return result;
 }
-const outputMultiply = multiply(2, 4);
-console.log(outputMultiply);
+// const outputMultiply = multiply(2, 4);
+// console.log(outputMultiply);
+
+let a = 4;
+let b = 5;
+let result = sum(a, b) + multiply(a, b);
+console.log(result);
