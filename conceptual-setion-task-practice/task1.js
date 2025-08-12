239bdf72de5fa776f7e17042260f8aab026ef151
sucharita4867 <sucharitasardar7@gmail.com>
2025-08-12 21:24:35 +0530

    added to day coseptual setion practice task

---
+++ conceptual-setion-task-practice/task1.js
@@ -0,0 +1,13 @@
+function sum(num1, num2) {
+  let result = num1 + num2;
+  return result;
+}
+const outputSum = sum(2, 4);
+console.log(outputSum);
+
+function multiply(num1, num2) {
+  let result = num1 * num2;
+  return result;
+}
+const outputMultiply = multiply(2, 4);
+console.log(outputMultiply);
