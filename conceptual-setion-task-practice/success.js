eedf3f9f0d087d2d649bd2fc42d2b2131ab5816e
sucharita4867 <sucharitasardar7@gmail.com>
2025-08-16 15:23:16 +0530

    this task is complete

--- conceptual-setion-task-practice/success.js
+++ conceptual-setion-task-practice/success.js
@@ -1,5 +1,24 @@
 function willSuccess(marks) {
-  console.log(marks);
+  if (Array.isArray(marks) === false) {
+    return "Invalid";
+  }
+  let totalno = 0;
+  let passno = 0;
+  let failno = 0;
+  for (const mark of marks) {
+    totalno += mark;
+    if (mark >= 50) {
+      passno++;
+    } else {
+      failno++;
+    }
+  }
+  if (passno > failno) {
+    return true;
+  }
+  if (failno > passno || failno === passno) {
+    return false;
+  }
 }
-const result = willSuccess([60, 70, 80, 40, 30]);
+const result = willSuccess(90);
 console.log(result);
