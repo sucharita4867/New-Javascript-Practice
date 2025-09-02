85076f4a2d1eeca656ef88c7e6200420eab49221
sucharita4867 <sucharitasardar7@gmail.com>
2025-09-02 23:22:19 +0530

    added truthy and falsy value practice

---
+++ JS-Core-Consept/truthy.js
@@ -0,0 +1,9 @@
+let number = 34;
+// const numbers = [];
+console.log(number);
+
+if (number) {
+  console.log("this is truthy value");
+} else {
+  console.log("this is falsy value");
+}
--- JS-Core-Consept/undifind.js
+++ JS-Core-Consept/undifind.js
@@ -1,2 +1,5 @@
 const number = [3, 3, 5, 6, 3];
-console.log(number[9]);
+// console.log(number[9]);
+
+const phone = { name: "sumsong", color: "white" };
+console.log(phone.price);
