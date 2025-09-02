057da072962c209b3ff6c0af81a92b5b098562e0
sucharita4867 <sucharitasardar7@gmail.com>
2025-09-02 23:43:48 +0530

    added this practice

--- JS-Core-Consept/equal.js
+++ JS-Core-Consept/equal.js
@@ -1,4 +1,7 @@
 // console.log(2 == 2);
 // console.log(2 === "2");
 // console.log([] == []);
-console.log({} == []);
+// console.log({} == []);
+// console.log(2 === false);
+console.log("3" == 3);
+console.log(true == 3);
